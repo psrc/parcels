c279a7719415fabf5fec9adfbde8714660077831
Christy Lam <cyyllam@gmail.com>
2017-05-04 18:24:14 -0700

    modified feature paths; included closest option for some spatial joins

--- spatial/prcltbl_spatialjoin.py
+++ spatial/prcltbl_spatialjoin.py
@@ -6,26 +6,30 @@ import arcpy, os, time
 start = time.time()
 
 # Dictionary containing joining features
-def shpDict(x):
+def shpDictWhole(x):
     return {
         'uga': 'W:\\geodata\\political\\PSRC_region.gdb\uga',
-        'juris': 'W:\\geodata\\political\\PSRC_region.gdb\psrc_region',
-        'zipcode': 'W:\\geodata\\political\\zipcode.shp',
-        'schooldist': 'J:\\Projects\\UrbanSim\\NEW_DIRECTORY\\GIS\\Shapefiles\\Political\\school_districts_WA.shp',
+        'schooldist': 'J:\\Staff\\Christy\\baseyear\\shapes\\overlay_shapes.gdb\school_districts',
         'urbcen': 'W:\\geodata\\political\\urbcen.shp',
         'micen': 'W:\\geodata\\political\\micen.shp',
-        'taz': 'W:\\geodata\\forecast\\taz2010_to_tad2010.shp',
-        'faz': 'W:\\geodata\\forecast\\FAZ_2010.shp',
-        'tract': 'W:\\geodata\\census\\Tract\\tract2010.shp',
-        'blockgp': 'W:\\geodata\\census\\Blockgroup\\blockgrp2010.shp',
-        'block': 'W:\\geodata\\census\\Block\\block2010.shp'
+    }[x]
+    
+def shpDict(x):
+    return {
+        'juris': 'psrc_region',
+        'zipcode': 'zipcode',
+        'taz': 'taz2010_to_tad2010',
+        'faz': 'FAZ_2010',
+        'tract': 'tract2010',
+        'blockgp': 'blockgrp2010',
+        'block': 'block2010'
     }[x]
 
 def oldFieldsDict(x):
     return {
         'uga': ['Join_Count'],
         'juris': [],
-        'zipcode': ['ZCTA5CE10'],
+        'zipcode': [], 
         'schooldist': ['NAME'],
         'urbcen': ['NAME'],
         'micen': [],
@@ -40,7 +44,7 @@ def newFieldsDict(x):
     return {
         'uga': ['IN_UGA'],
         'juris': [],
-        'zipcode': ['ZIP'],
+        'zipcode': [],
         'schooldist': ['DISTNAME'],
         'urbcen': ['URBCEN'],
         'micen': [],
@@ -67,15 +71,19 @@ def keepFieldsDict(x):
     }[x]
     
 # Environment inputs  
-counties = ['Kitsap', 'Pierce', 'Snohomish', 'King'] 
-shp0 = ['kitptfnl15testCopy.shp','pieptfnl15testCopy.shp', 'snoptfnl15testCopy.shp', 'kinptfnl15testCopy.shp']
-joinFeaturesList = ['uga', 'juris', 'zipcode', 'schooldist', 'urbcen', 'micen', 'taz', 'faz', 'tract', 'blockgp', 'block']
+counties = ['Kitsap', 'Pierce', 'Snohomish']#, 'King'
+shp0 = ['kitptfnl15testCopy.shp','pieptfnl15testCopy.shp', 'snoptfnl15testCopy.shp']#, 'kinptfnl15testCopy.shp'
+subsettedFc = ['juris', 'zipcode',  'taz', 'faz', 'tract', 'blockgp', 'block']
+wholeFc = ['uga', 'schooldist', 'urbcen', 'micen']
+joinFeaturesList = subsettedFc + wholeFc
+
 target = ["overlay"+str(i) for i in range(1, len(joinFeaturesList)+1)]
 
 rootDir = 'J:\\Projects\\UrbanSim\\NEW_DIRECTORY\\GIS\\Shapefiles\\Parcels'
-homeDir = '2015_test'
+homeDir = '2015'
 shp0Dir = 'parcels_table'
 outshpDir = 'spatial_overlay'
+joinFeaturesGdb = 'layers.gdb'
 
 # Iterate for number of counties
 for c in range(len(counties)):    
@@ -94,36 +102,34 @@ for c in range(len(counties)):
         if n == 0:
             targetFeatures = os.path.join(workspace, shp0[c])
         else:
-            targetFeatures = os.path.join(outWorkspace, target[n-1])
-        
+            targetFeatures = os.path.join(outWorkspace, target[n-1])       
         # List current target feature fields
         keepFieldsList = [f.name for f in arcpy.ListFields(targetFeatures)]
-        
         outfc = os.path.join(outWorkspace, target[n])
-        joinFeatures = shpDict(joinFeaturesList[n]) 
-        
-        # Spatial join
-        arcpy.SpatialJoin_analysis(targetFeatures, joinFeatures, outfc, join_operation = "JOIN_ONE_TO_ONE", join_type = "KEEP_ALL")  
-        
+        if joinFeaturesList[n] in subsettedFc:
+            joinFeatures = os.path.join(rootDir, counties[c], homeDir, outshpDir, joinFeaturesGdb, shpDict(joinFeaturesList[n])) 
+        else:
+            joinFeatures = shpDictWhole(joinFeaturesList[n])
+        # Spatial join; exclude 'closest' match option for 'uga', 'urbcen' and 'micen' features
+        if joinFeaturesList[n] in subsettedFc + ['schooldist']:
+            arcpy.SpatialJoin_analysis(targetFeatures, joinFeatures, outfc, join_operation = "JOIN_ONE_TO_ONE", join_type = "KEEP_ALL", match_option = "CLOSEST")
+        else:
+            arcpy.SpatialJoin_analysis(targetFeatures, joinFeatures, outfc, join_operation = "JOIN_ONE_TO_ONE", join_type = "KEEP_ALL")        
         # Rename fields
         oldFields = oldFieldsDict(joinFeaturesList[n])
-        newFields = newFieldsDict(joinFeaturesList[n])
-        
+        newFields = newFieldsDict(joinFeaturesList[n])       
         for i in range(len(oldFields)):
-            arcpy.AlterField_management(outfc, oldFields[i], newFields[i])
-        
+            arcpy.AlterField_management(outfc, oldFields[i], newFields[i])        
         # Extend list of 'keep' fields
         if n == 0:
             keepFieldsList.extend(requiredFieldsList)
         keepFieldsList.extend(keepFieldsDict(joinFeaturesList[n]))
         # Re-list target features fields
         fields = arcpy.ListFields(outfc)
-        dropFields = [f.name for f in fields if f.name not in keepFieldsList]        
-        
+        dropFields = [f.name for f in fields if f.name not in keepFieldsList]                
         # Delete fields
-        arcpy.DeleteField_management(outfc, dropFields)
-        
+        arcpy.DeleteField_management(outfc, dropFields)       
         print "Spatial joined " + counties[c] + " " + joinFeaturesList[n]
 
 end = time.time()
-print(str((end-start)/60) + " minutes")
+print(str(((end-start)/60)/60) + " hours")
